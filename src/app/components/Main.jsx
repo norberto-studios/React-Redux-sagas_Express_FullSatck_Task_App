962ad36363ac9487672a4d66a9f9ec0ccc5b5c86
Norberto Fernandez <Norberto@Norbertos-MacBook-Pro.local>
2020-04-17 02:16:47 -0400

    Added navigation and history

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "./Dashboard";
+import { Router, Route } from "react-router-dom";
+import { history } from "../store/history";
+import { ConnectedNavigation } from "./Navigation";
 
 export const Main = () => (
-  <Provider store={store}>
-    <ConnectedDashboard />
-  </Provider>
+  <Router history={history}>
+    <Provider store={store}>
+      <ConnectedNavigation />
+      <Route exact path="/dashboard" render={() => <ConnectedDashboard />} />
+    </Provider>
+  </Router>
 );
